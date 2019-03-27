6532ca291281b8de065950e0a6be0fb4160c4f6b
Leeya Davis <leeya463@gmail.com>
2019-03-27 03:02:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
 
     holidays.collect do |holiday, supplies|
-      puts holiday if supplies.include?("BBQ")
+      holiday.key("BBQ")
       #binding.pry
     end
     holidays
