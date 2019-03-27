896db67911c1709e9e3b015dc4d77de020e8bb96
Leeya Davis <leeya463@gmail.com>
2019-03-27 02:52:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
-    holidays.select do |holiday, supplies|
+    holidays.collect do |holiday, supplies|
       #binding.pry
       holiday if supplies.include?("BBQ")
       #binding.pry
