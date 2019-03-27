3ccf3e06ee005f9c39ab86e00b741be21876a8af
Leeya Davis <leeya463@gmail.com>
2019-03-27 02:00:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{season}:".to_s.capitalize
+    puts "#{season}:".to_s.capitalize.chomp
       #<--holidays.collect do |holiday, supplies|
         #still |symbol,  array|
         #holiday = holiday.to_s.gsub("_", " ").split
